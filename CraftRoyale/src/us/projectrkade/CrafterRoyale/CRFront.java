b367e114e1f310156cde96375949f425fe414aef
Lucas Eastman <lucas546alexander@gmail.com>
2018-03-24 21:53:34 -0700

    Created CRFront; Added CraftBukkit import;
    Created CRFront and extended JavaPlugin; Added onEnable() method to set
    values for static plugDir File object (Plugin Directory) and config file
    (config.yml) and load and save the configuration file;

---
+++ CraftRoyale/src/us/projectrkade/CrafterRoyale/CRFront.java
@@ -0,0 +1,36 @@
+package us.projectrkade.CrafterRoyale;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.bukkit.Bukkit;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class CRFront extends JavaPlugin {
+	
+	public static File plugDir;
+	public static File config;
+	
+	public void onEnable() {
+		plugDir = Bukkit.getServer().getPluginManager().getPlugin("CrafterRoyale").getDataFolder();
+		if(!plugDir.exists()) {
+			plugDir.mkdir();
+		}
+		config = new File(plugDir.getPath() + File.separator + "config.yml");
+		if(!config.exists()) {
+			try {
+				config.createNewFile();
+			} catch (IOException e) {
+				System.out.println(e.getMessage());
+			}
+		}
+		YamlConfiguration conYAML = YamlConfiguration.loadConfiguration(config);
+		try {
+			conYAML.save(config);
+		} catch (IOException e) {
+			System.out.out.println(e.getMessage());
+		}
+	}
+	
+}
