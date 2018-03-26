d9a2b7ea6da1066ad9054de4b40012a45e7c92d7
Lucas Eastman <lucas546alexander@gmail.com>
2018-03-25 23:41:35 -0700

    Created Commands.java; Added and set executor for /cr command;
    Created Commands.java for command execution; Added switch for command in
    Commands.java, with a switch for cr and its lobby create and remove
    commands so far, which add or remove lobby information from settings.yml
    respectively;

--- CraftRoyale/src/us/projectrkade/CrafterRoyale/CRFront.java
+++ CraftRoyale/src/us/projectrkade/CrafterRoyale/CRFront.java
@@ -4,17 +4,21 @@ import java.io.File;
 import java.io.IOException;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class CRFront extends JavaPlugin {
 	
+	public static String prefix;
 	public static File plugDir;
 	public static File config;
 	public static File settings;
 	
 //	onEnable() method invoke on plugin enable
 	public void onEnable() {
+//		Set prefix String to correct plugin prefix
+		prefix = "" + ChatColor.GRAY + "[" + ChatColor.YELLOW + "Crafter" + ChatColor.GREEN + "Royale" + ChatColor.GRAY + "]" + ChatColor.RESET + "";
 //		Set static plugDir object to the "CrafterRoyale" plugin's data folder file
 		plugDir = Bukkit.getServer().getPluginManager().getPlugin("CrafterRoyale").getDataFolder();
 //		Check to see if a file or directory matching the plugDir File object path exists
@@ -69,6 +73,7 @@ public class CRFront extends JavaPlugin {
 //			Print error message to console
 			System.out.println(e.getMessage());
 		}
+		Bukkit.getServer().getPluginCommand("cr").setExecutor(new Commands());
 	}
 	
 }
---
+++ CraftRoyale/src/us/projectrkade/CrafterRoyale/Commands.java
@@ -0,0 +1,99 @@
+package us.projectrkade.CrafterRoyale;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.Player;
+
+public class Commands implements CommandExecutor{
+
+	@Override
+	public boolean onCommand(CommandSender sndr, Command cmd, String label, String[] args) {
+		if(!(sndr instanceof Player)) {
+			sndr.sendMessage("Command can not be executed by console personnel. Please try again in-game.");
+			return true;
+		}
+		Player player = (Player) sndr;
+		YamlConfiguration setYAML = YamlConfiguration.loadConfiguration(CRFront.settings);
+		switch(cmd.getName().toLowerCase()) {
+		case "cr":
+			if(!sndr.hasPermission("cr.base")) {
+				sndr.sendMessage(CRFront.prefix + ChatColor.RED + " You do not have the correct permission to use this command.");
+				return true;
+			}
+			if(args.length < 1) {
+//				Display substitute menu
+				return true;
+			}
+			switch(args[0]) {
+			case "admin":
+				if(!sndr.hasPermission("cr.admin")) {
+					sndr.sendMessage(CRFront.prefix + ChatColor.RED + " You do not have the correct permission to use this command.");
+					return true;
+				}
+				if(args.length < 2) {
+//					Display substitute menu
+					return true;
+				}
+				switch(args[1]) {
+				case "lobby":
+					if(!sndr.hasPermission("cr.admin.lobby")) {
+						sndr.sendMessage(CRFront.prefix + ChatColor.RED + " You do not have the correct permission to use this command.");
+						return true;
+					}
+					if(args.length < 3) {
+//						Display substitute menu
+						return true;
+					}
+					switch(args[2]) {
+					case "create":
+						if(!sndr.hasPermission("cr.admin.lobby.create")) {
+							sndr.sendMessage(CRFront.prefix + ChatColor.RED + " You do not have the correct permission to use this command.");
+							return true;
+						}
+						if(args.length < 4) {
+//							Display correct usage
+							return true;
+						}
+//						/cr admin lobby create <name>
+						String toCreate = args[3];
+						if(setYAML.contains("lobbies." + toCreate)) {
+							sndr.sendMessage(CRFront.prefix + ChatColor.RED + " Lobby " + ChatColor.DARK_RED + toCreate + " already exists.");
+							return true;
+						}
+						setYAML.createSection("lobbies." + toCreate);
+						setYAML.set("lobbies." + toCreate + ".world", player.getWorld().getName());
+						setYAML.set("lobbies." + toCreate + ".location.x", player.getLocation().getX());
+						setYAML.set("lobbies." + toCreate + ".location.y", player.getLocation().getY());
+						setYAML.set("lobbies." + toCreate + ".location.z", player.getLocation().getZ());
+						setYAML.set("lobbies." + toCreate + ".location.yaw", player.getLocation().getYaw());
+						setYAML.set("lobbies." + toCreate + ".location.pitch", player.getLocation().getPitch());
+						sndr.sendMessage(CRFront.prefix + ChatColor.DARK_RED + " Lobby " + ChatColor.DARK_RED + toCreate + " created.");
+						break;
+					case "remove":
+						if(!sndr.hasPermission("cr.admin.lobby.remove")) {
+							sndr.sendMessage(CRFront.prefix + ChatColor.RED + " You do not have the correct permission to use this command.");
+							return true;
+						}
+						if(args.length < 4) {
+//							Display correct usage
+							return true;
+						}
+						String toRemove = args[3];
+						if(!setYAML.contains("lobbies." + toRemove)) {
+							sndr.sendMessage(CRFront.prefix + ChatColor.RED + " Lobby" + ChatColor.DARK_RED + toRemove + " does not exist.");
+							return true;
+						}
+						setYAML.set("lobbies." + toRemove, "");
+						sndr.sendMessage(CRFront.prefix + ChatColor.DARK_RED + " Lobby " + ChatColor.DARK_RED + toRemove + " created.");
+						break;
+					}
+				}
+			}
+		}
+		return true;
+	}
+
+}
