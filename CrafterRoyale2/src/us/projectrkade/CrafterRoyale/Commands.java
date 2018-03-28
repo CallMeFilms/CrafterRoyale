107a555a3cd9a654c0ec3ef0734666229b9a9845
Lucas Eastman <lucas546alexander@gmail.com>
2018-03-27 23:55:29 -0700

    Updated cr command and its messages
    Updated "/cr" command by removing the "admin" argument and replacing it
    with "lobby", and updated messages for execution of "/cr" command

--- CrafterRoyale2/src/us/projectrkade/CrafterRoyale/Commands.java
+++ CrafterRoyale2/src/us/projectrkade/CrafterRoyale/Commands.java
@@ -9,7 +9,7 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 
-public class Commands implements CommandExecutor{
+public class Commands implements CommandExecutor {
 
 	@Override
 	public boolean onCommand(CommandSender sndr, Command cmd, String label, String[] args) {
@@ -22,88 +22,87 @@ public class Commands implements CommandExecutor{
 		switch(cmd.getName().toLowerCase()) {
 		case "cr":
 			if(!sndr.hasPermission("cr.base")) {
-				sndr.sendMessage(CRFront.prefix + ChatColor.RED + " You do not have the correct permission to use this command.");
+				sndr.sendMessage(CRFront.prefix + ChatColor.YELLOW + " You do not have the correct permission to use this command.");
 				return true;
 			}
 			if(args.length < 1) {
-//				Display substitute menu
+				sndr.sendMessage(ChatColor.YELLOW + "-------" + CRFront.prefix + ChatColor.YELLOW + "-------");
+				sndr.sendMessage(ChatColor.GOLD + "/cr help: " + ChatColor.YELLOW + "Displays this menu.");
+				sndr.sendMessage(ChatColor.GOLD + "/cr admin: " + ChatColor.YELLOW + "Used for all CrafterRoyale in-game administration.");
 				return true;
 			}
 			switch(args[0]) {
 			case "admin":
 				if(!sndr.hasPermission("cr.admin")) {
-					sndr.sendMessage(CRFront.prefix + ChatColor.RED + " You do not have the correct permission to use this command.");
+					sndr.sendMessage(CRFront.prefix + ChatColor.YELLOW + " You do not have the correct permission to use this command.");
 					return true;
 				}
 				if(args.length < 2) {
-//					Display substitute menu
+					sndr.sendMessage(ChatColor.YELLOW + "-------" + CRFront.prefix + ChatColor.YELLOW + "-------");
+					sndr.sendMessage(ChatColor.GOLD + "/cr admin lobby: " + ChatColor.YELLOW + "Used for managing lobbies and their settings.");
+					return true;
+				}
+				break;
+			case "lobby":
+				if(args.length < 2) {
+					sndr.sendMessage(CRFront.prefix + ChatColor.GOLD + " Correct usage: " + ChatColor.YELLOW + "/lobby <create:remove> <lobbyname>");
 					return true;
 				}
 				switch(args[1]) {
-				case "lobby":
-					if(!sndr.hasPermission("cr.admin.lobby")) {
-						sndr.sendMessage(CRFront.prefix + ChatColor.RED + " You do not have the correct permission to use this command.");
-						return true;
-					}
+				case "create":
 					if(args.length < 3) {
-//						Display substitute menu
+						sndr.sendMessage(CRFront.prefix + ChatColor.GOLD + " Correct usage: " + ChatColor.YELLOW + "/lobby create <name>");
 						return true;
 					}
-					switch(args[2]) {
-					case "create":
-						if(!sndr.hasPermission("cr.admin.lobby.create")) {
-							sndr.sendMessage(CRFront.prefix + ChatColor.RED + " You do not have the correct permission to use this command.");
-							return true;
-						}
-						if(args.length < 4) {
-//							Display correct usage
-							return true;
-						}
-//						/cr admin lobby create <name>
-						String toCreate = args[3];
-						if(setYAML.contains("lobbies." + toCreate)) {
-							sndr.sendMessage(CRFront.prefix + ChatColor.DARK_RED + " Lobby " + ChatColor.RED + toCreate + ChatColor.DARK_RED + " already exists.");
-							return true;
-						}
-						setYAML.createSection("lobbies." + toCreate);
-						setYAML.set("lobbies." + toCreate + ".world", player.getWorld().getName());
-						setYAML.set("lobbies." + toCreate + ".location.x", player.getLocation().getX());
-						setYAML.set("lobbies." + toCreate + ".location.y", player.getLocation().getY());
-						setYAML.set("lobbies." + toCreate + ".location.z", player.getLocation().getZ());
-						setYAML.set("lobbies." + toCreate + ".location.yaw", player.getLocation().getYaw());
-						setYAML.set("lobbies." + toCreate + ".location.pitch", player.getLocation().getPitch());
-						try {
-							setYAML.save(CRFront.settings);
-						} catch (IOException e) {
-							System.out.println(e.getMessage());
-						}
-						sndr.sendMessage(CRFront.prefix + ChatColor.DARK_RED + " Lobby " + ChatColor.RED + toCreate + ChatColor.DARK_RED + " created.");
-						break;
-					case "remove":
-						if(!sndr.hasPermission("cr.admin.lobby.remove")) {
-							sndr.sendMessage(CRFront.prefix + ChatColor.RED + " You do not have the correct permission to use this command.");
-							return true;
-						}
-						if(args.length < 4) {
-//							Display correct usage
-							return true;
-						}
-						String toRemove = args[3];
-						if(!setYAML.contains("lobbies." + toRemove)) {
-							sndr.sendMessage(CRFront.prefix + ChatColor.DARK_RED + " Lobby " + ChatColor.RED + toRemove + ChatColor.DARK_RED + " does not exist.");
-							return true;
-						}
-						setYAML.set("lobbies." + toRemove, null);
-						try {
-							setYAML.save(CRFront.settings);
-						} catch (IOException e) {
-							System.out.println(e.getMessage());
-						}
-						sndr.sendMessage(CRFront.prefix + ChatColor.DARK_RED + " Lobby " + ChatColor.RED + toRemove + ChatColor.DARK_RED + " removed.");
-						break;
+					String toCreate = args[2];
+					if(setYAML.contains("lobbies." + toCreate)) {
+						sndr.sendMessage(CRFront.prefix + ChatColor.GOLD + " Lobby " + ChatColor.YELLOW + toCreate + ChatColor.GOLD + " already exists.");
+						return true;
 					}
+					setYAML.createSection("lobbies." + toCreate);
+					setYAML.set("lobbies." + toCreate + ".world", player.getWorld().getName());
+					setYAML.set("lobbies." + toCreate + ".location.x", player.getLocation().getX());
+					setYAML.set("lobbies." + toCreate + ".location.y", player.getLocation().getY());
+					setYAML.set("lobbies." + toCreate + ".location.z", player.getLocation().getZ());
+					setYAML.set("lobbies." + toCreate + ".location.yaw", player.getLocation().getYaw());
+					setYAML.set("lobbies." + toCreate + ".location.pitch", player.getLocation().getPitch());
+					try {
+						setYAML.save(CRFront.settings);
+					} catch (IOException e) {
+						System.out.println(e.getMessage());
+					}
+					sndr.sendMessage(CRFront.prefix + ChatColor.GOLD + " Lobby " + ChatColor.YELLOW + toCreate + ChatColor.GOLD + " created.");
+					break;
+				case "remove":
+					if(args.length < 3) {
+						sndr.sendMessage(CRFront.prefix + ChatColor.GOLD + " Correct usage: " + ChatColor.YELLOW + "/lobby remove <name>");
+						return true;
+					}
+					String toRemove = args[2];
+					if(!setYAML.contains("lobbies." + toRemove)) {
+						sndr.sendMessage(CRFront.prefix + ChatColor.GOLD + " Lobby " + ChatColor.YELLOW + toRemove + ChatColor.GOLD + " does not exist.");
+						return true;
+					}
+					setYAML.set("lobbies." + toRemove, null);
+					try {
+						setYAML.save(CRFront.settings);
+					} catch (IOException e) {
+						System.out.println(e.getMessage());
+					}
+					sndr.sendMessage(CRFront.prefix + ChatColor.GOLD + " Lobby " + ChatColor.YELLOW + toRemove + ChatColor.GOLD + " removed.");
+					break;
+				default:
+					sndr.sendMessage(CRFront.prefix + ChatColor.GOLD + " Correct usage: " + ChatColor.YELLOW + "/lobby remove <name>");
+					break;
 				}
+				break;
+			default:
+				sndr.sendMessage(ChatColor.YELLOW + "-------" + CRFront.prefix + ChatColor.YELLOW + "-------");
+				sndr.sendMessage(ChatColor.GOLD + "/cr help: " + ChatColor.YELLOW + "Displays this menu.");
+				sndr.sendMessage(ChatColor.GOLD + "/cr lobby <create:remove> <lobbyname>: " + ChatColor.YELLOW + "Create or remove a lobby.");
+				break;
 			}
+			break;
 		}
 		return true;
 	}
